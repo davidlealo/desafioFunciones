94c4a75f9f01d4c00da602b1a2f02f58f6dfb9df
davidlealo <davidlealo@gmail.com>
2022-07-10 16:10:48 -0400

    Primera sección de la cuarta parte del desafío

--- DescripcionSoluciones.js
+++ DescripcionSoluciones.js
@@ -53,4 +53,31 @@ tener que seleccionarlo nuevamente dentro de la función (1 Punto). Para
 obtener el puntaje debes entregar el 3 pintar.html válido funcionando en
 conjunto con el código modificado.
 
+3.2. Modifica el código anterior para poder pasarle un color como argumento a la
+función pintar. El color debe ser verde (green) por defecto, al hacer clic en el
+párrafo se debe pasar amarillo como color. (1 Punto).
+3.3. Separa el código en 2 archivos: 3 pintar.html y script.js. El script js debe estar
+dentro de la carpeta assets/js (1 Punto). Para obtener el puntaje debes
+entregar los archivos cumplimiento la estructura pedida en conjunto con los
+requerimientos de 3.1 y 3.2
+
+*/
+
+//Respuesta entre pintar.html y sus css/js
+
+/*
+4. Construye una página web con las siguientes características (Total: 5 puntos)
+● Crea 4 divs que tengan alto y ancho de 200px y de distintos colores de fondo
+usando el atributo style. Cada uno de los divs debe tener un identificador
+único.
+● Crea un script que guarde dentro de una variable global un color
+dependiendo de la letra del teclado presionada. (2 Puntos).
+○ Al presionar la letra a un color a tu elección.
+○ Al presionar la letra s un segundo color a tu elección.
+○ Al presionar la letra d un tercer color al presionar la letra d.
+○ Para guardar el color revisa el tip al final del enunciado.
+● Dentro del script agrega el evento que al hacerle click a uno de los divs, este
+cambie de color al color seleccionado. Utiliza addEventListener para agregar
+el evento. (3 Puntos)
+
 */
